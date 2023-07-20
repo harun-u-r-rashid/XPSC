d1c5a5bbef6b35150ac600460b0e4de459584eda
harun-u-r-rashid <hr100756@gmail.com>
2023-07-20 19:57:28 +0600

    Update

--- 6th week/Day 06/Largest and Second Largest.cpp
+++ 6th week/Day 06/Largest and Second Largest.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int test; cin>>test;
+
+    while(test--)
+    {
+        int n; cin>>n;
+
+        vector<int>arr(n);
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+
+
+        sort(arr.begin(),arr.end());
+        int result;
+
+        for(int i=0; i<n-1; i++)
+        {
+            if(arr[i]!=arr[n-1])
+                result = arr[i];
+        }
+
+        cout<<result+arr[n-1]<<"\n";
+    }
+}
