3c3654735c15087e65e9f0e1492abf5b713c4011
harun-u-r-rashid <119634853+harun-u-r-rashid@users.noreply.github.com>
2023-07-29 12:58:43 +0600

    Update Construct String.cpp

--- 8th week/Day 01/Construct String.cpp
+++ 8th week/Day 01/Construct String.cpp
@@ -1,1 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        string s;
+        cin>>s;
+        string ans;
+
+        char ch = s[0];
+        int cnt = 1;
+
+        for(int i=1; i<n; i++)
+        {
+            if(ch == s[i])
+            {
+                cnt++;
+            }
+            else
+            {
+
+                if(cnt%2)
+                {
+
+                    ans.push_back(ch);
+                }
+                else
+                {
+                    ans.push_back(ch);
+                    ans.push_back(ch);
+                }
+                ch = s[i];
+                cnt = 1;
+            }
+        }
+
+        if(cnt%2)
+        {
+
+            ans.push_back(ch);
+        }
+        else
+        {
+            ans.push_back(ch);
+            ans.push_back(ch);
+        }
+
+
+
+        cout<<ans<<"\n";
+
+    }
+
+    return 0;
+}
 
