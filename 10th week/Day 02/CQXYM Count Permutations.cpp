7a7958a9d9698d4b9a6818083c9db20a84564eb3
harun-u-r-rashid <hr100756@gmail.com>
2023-08-12 22:51:19 +0600

    Update

---
+++ 10th week/Day 02/CQXYM Count Permutations.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin>>t; while(t--)
+    {
+
+        long long m  = 1e9 + 7;
+
+        int n; cin>>n;
+
+
+        long long  ans = 1;
+
+//        for(long long i = 2; i<=(n+n); i++)
+//        {
+//            ans = ans * i;
+//            ans = ans % m;
+//        }
+
+for(long long i = (n+n); i>=3; i--)
+{
+    ans = ans * i;
+    ans = ans % m;
+}
+
+        cout<<ans<<"\n";
+
+    }
+
+    return 0;
+}
---
+++ 10th week/Day 02/Consecutive Sum Riddle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main()
+{
+    int t; cin>>t; while(t--)
+    {
+        long long n; cin>>n;
+
+        long long ans = n - 1;
+
+        cout<<-(ans)<<" "<<n<<"\n";
+
+    }
+
+    return 0;
+}
