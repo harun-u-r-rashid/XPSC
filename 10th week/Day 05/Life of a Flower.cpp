3ea68ebc85e17d3956167c8023c369c844176069
harun-u-r-rashid <hr100756@gmail.com>
2023-08-15 23:54:35 +0600

    Update

---
+++ 10th week/Day 05/Life of a Flower.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n ;
+        cin>>n;
+
+        vector<int>a(n);
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+
+
+        int sum = 1;
+
+
+
+
+        for(int i=0; i<n; i++)
+
+        {
+            if(a[i] == 1)
+            {
+                if(i > 0 && a[i-1] == 1)
+                    sum += 5;
+                else sum++;
+            }
+
+            else
+            {
+                if(i > 0 && a[i-1] == 0)
+                {
+                    sum = -1;
+                    break;
+                }
+
+            }
+        }
+
+        cout<<sum<<"\n";
+
+
+    }
+    return 0;
+}
